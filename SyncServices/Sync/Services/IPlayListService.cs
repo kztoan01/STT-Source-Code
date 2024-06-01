03ade95979815864a60bfc7f4979a874db83a5bc
Nebular_Scripter <nguyphi974@gmail.com>
2024-06-01 09:35:12 +0700

    Add playlistcontroller

---
+++ SyncServices/Sync/Services/IPlayListService.cs
@@ -0,0 +1,18 @@
+﻿using Sync.DTOs;
+
+namespace Sync.Services
+{
+    public interface iPlayListService
+    {
+        PlaylistDTO AddPlaylist(PlaylistDTO playlistDTO);
+        PlaylistDTO UpdatePlaylist(PlaylistDTO playlistDTO);
+        void DeletePlaylist(Guid playlistId);
+        PlaylistDTO GetPlaylistById(Guid playlistId);
+        List<PlaylistDTO> GetAllPlaylists();
+
+        int GetTotalPlaylists();
+        int GetPlaylistsCreatedInLastMonth();
+        PlaylistDTO GetMostPopularPlaylist();
+        List<UserPlaylistDTO> GetUserPlaylistsByUserId(Guid userId);
+    }
+}
---
+++ SyncServices/Sync/Services/iml/PlayListService.cs
@@ -0,0 +1,60 @@
+﻿using Sync.DTOs;
+using Sync.Model;
+using Sync.Repository;
+
+namespace Sync.Services.iml
+{
+    public class PlayListService : IPlayListService
+    {
+        private IPlaylistRepository playlistRepository;
+        private readonly EFDataContext _context;
+        public PlayListService(EFDataContext context) {
+            _context = context;
+            playlistRepository = new PlaylistRepository(context);
+        }
+        public PlaylistDTO AddPlaylist(PlaylistDTO playlistDTO)
+        {
+          return  playlistRepository.AddPlaylist(playlistDTO);
+        }
+
+        public void DeletePlaylist(Guid playlistId)
+        {
+            playlistRepository.DeletePlaylist(playlistId);
+        }
+
+        public List<PlaylistDTO> GetAllPlaylists()
+        {
+            return playlistRepository.GetAllPlaylists();
+        }
+
+        public PlaylistDTO GetMostPopularPlaylist()
+        {
+           return playlistRepository.GetMostPopularPlaylist();
+        }
+
+        public PlaylistDTO GetPlaylistById(Guid playlistId)
+        {
+           return playlistRepository.GetPlaylistById(playlistId);
+        }
+
+        public int GetPlaylistsCreatedInLastMonth()
+        {
+            return playlistRepository.GetPlaylistsCreatedInLastMonth();
+        }
+
+        public int GetTotalPlaylists()
+        {
+           return playlistRepository.GetTotalPlaylists();
+        }
+
+        public List<UserPlaylistDTO> GetUserPlaylistsByUserId(Guid userId)
+        {
+            return playlistRepository.GetUserPlaylistsByUserId(userId);
+        }
+
+        public PlaylistDTO UpdatePlaylist(PlaylistDTO playlistDTO)
+        {
+            return playlistRepository.UpdatePlaylist(playlistDTO);
+        }
+    }
+}
