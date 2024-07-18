c2651a2f959b17c7be1c016984d415df063fe51f
NhanDz <lmnhn995@gmail.com>
2024-07-18 13:05:03 +0700

    crud genre

--- SyncService/repository/Repository/GenreRepository.cs
+++ SyncService/repository/Repository/GenreRepository.cs
@@ -14,8 +14,40 @@ public class GenreRepository : IGenreRepository
         _context = context;
     }
 
+    public async Task<Genre> CreateGenreAsync(Genre genre)
+    {
+        _context.Genres.AddAsync(genre); 
+        await _context.SaveChangesAsync();
+        return genre;
+    }
+
+    public async Task<bool> DeleteGenreByIdAsync(Guid id)
+    {
+        var genre = await _context.Genres.FirstOrDefaultAsync(g => g.Id == id);
+        if (genre == null)
+        {
+            return false;
+        }
+        _context.Genres.Remove(genre);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
     public async Task<List<Genre>> GetAllGenre()
     {
         return await _context.Genres.ToListAsync();
+    }
+
+    public async Task<Genre> GetGenreById(Guid id)
+    {
+        var genre = await _context.Genres.FirstOrDefaultAsync(g => g.Id == id);
+        return genre;
+    }
+
+    public async Task<Genre> UpdateGenreAsync(Genre genre)
+    {
+       _context.Genres.Update(genre);
+        await _context.SaveChangesAsync();
+        return genre;
     }
 }
